bbc6b41a3fd3336ab9e44cb54a4773ce6ea3d40c
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-11-10 03:00:22 +0530

    done

---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+
+
+export default function Welcome() {
+  return (
+    <>
+    <div className='container1'>
+
+    <h1>Welcome to <strong className='code'>CodePen</strong> Online Editor <i className="fa-brands code fa-codepen"></i></h1>
+    
+     </div>
+    </>
+  )
+}
